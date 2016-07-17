825d260b89e261ec7a9698a6eda962d74ca403c5
Jonathan Guess <jqguess@liberty.edu>
2016-07-16 23:04:18 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+     $("button").click(function() {
+        var allPElements = $( ".phide" );
+        $( ".phide" ).find( allPElements );
+         $(".phide").toggle();
+     });
+  
+
